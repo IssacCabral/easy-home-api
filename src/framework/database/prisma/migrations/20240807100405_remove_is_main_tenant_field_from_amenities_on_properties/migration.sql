ab476089f2399c5a0518fdbd287f597365805c5b
Issac Cabral <clidenorissac@gmail.com>
2024-08-07 07:05:11 -0300

    🐞 fix: remove is_main_tenant field from amenities on properties

---
+++ src/framework/database/prisma/migrations/20240807100405_remove_is_main_tenant_field_from_amenities_on_properties/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `isMainTenant` on the `AmenitiesOnProperties` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "AmenitiesOnProperties" DROP COLUMN "isMainTenant";
