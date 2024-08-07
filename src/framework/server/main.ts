877180ced0bdf561476a6fedc0777dd74626a194
Issac Cabral <clidenorissac@gmail.com>
2024-08-07 17:08:50 -0300

    🐳 chore: remove log routes

--- src/framework/server/main.ts
+++ src/framework/server/main.ts
@@ -1,6 +1,5 @@
 import express, { json } from "express";
 import { routes } from "./routes/routes";
-import { logRoutes } from "./config/logRoutes";
 
 const SERVER_PORT: number = Number(process.env.PORT || 3000);
 const app = express();
@@ -10,5 +9,4 @@ app.use(routes);
 
 app.listen(SERVER_PORT, () => {
 	console.log(`Server Running on port ${SERVER_PORT}`);
-	// logRoutes(routes);
 });
