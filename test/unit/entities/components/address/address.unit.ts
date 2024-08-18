4d30764538252526ea5350ccd3d0140aa54d65e6
Issac Cabral <clidenorissac@gmail.com>
2024-08-18 06:29:10 -0300

    🧪 test: should fail if invalid coordinates is provided

--- test/unit/entities/components/address/address.unit.ts
+++ test/unit/entities/components/address/address.unit.ts
@@ -1,7 +1,19 @@
 import { AddressEntity } from "@entities/components/address/address";
+import { InvalidCoordinates } from "@entities/errors/address";
 import { fakeAddressEntity } from "@test/utility/fakes/addressEntity";
 
 describe("AddressEntity", () => {
+	it("should fail if invalid coordinates is provided", () => {
+		const addressEntity = AddressEntity.create({
+			...fakeAddressEntity,
+			lat: 100,
+		});
+
+		expect(addressEntity.isLeft()).toBeTruthy();
+		expect(addressEntity.isRight()).toBeFalsy();
+		expect(addressEntity.value).toEqual(InvalidCoordinates);
+	});
+
 	it("should create on success", () => {
 		const addressEntity = AddressEntity.create(fakeAddressEntity);
 
