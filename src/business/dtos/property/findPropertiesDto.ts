1946c6795db02d7d0e0163a690142d1707ebff1f
Issac Cabral <clidenorissac@gmail.com>
2024-10-15 13:10:46 -0300

    🐳 chore: type find properties dto

--- src/business/dtos/property/findPropertiesDto.ts
+++ src/business/dtos/property/findPropertiesDto.ts
@@ -1,4 +1,4 @@
-import type { IPropertyEntity } from "@entities/components/property/property";
+import type { IPropertyEntity, PropertyStatus, PropertyTypes } from "@entities/components/property/property";
 import type { PaginationData, PaginationParams } from "@entities/shared/pagination";
 import type { Either } from "@shared/either";
 import type { IError } from "@shared/iError";
@@ -7,6 +7,13 @@ export type InputFindPropertiesDto = {
 	centralLat: number;
 	centralLon: number;
 	radiusInMeters: number;
+	minPrice?: number;
+	maxPrice?: number;
+	minBedrooms?: number;
+	maxBedrooms?: number;
+	status?: PropertyStatus;
+	type?: PropertyTypes;
+	amenities?: string[];
 } & PaginationParams;
 
 export type OutputFindPropertiesDto = Either<IError, PaginationData<IPropertyEntity>>;
