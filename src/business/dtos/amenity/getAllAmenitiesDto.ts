0c031448994a597b16712932189e9866ec1bc75c
Issac Cabral <clidenorissac@gmail.com>
2024-10-18 09:34:01 -0300

    🐳 chore: get all amenities dto and repository

--- src/business/dtos/amenity/getAllAmenitiesDto.ts
+++ src/business/dtos/amenity/getAllAmenitiesDto.ts
@@ -2,7 +2,4 @@ import type { IAmenityEntity } from "@entities/components/amenity/amenity";
 import type { Either } from "@shared/either";
 import type { IError } from "@shared/iError";
 
-export interface IAmenityRepository {
-	findByIds(ids: string[]): Promise<Either<IError, IAmenityEntity[]>>;
-	getAll(): Promise<IAmenityEntity[]>;
-}
+export type OutputGetAllAmenitiesDto = Either<IError, IAmenityEntity[]>;
