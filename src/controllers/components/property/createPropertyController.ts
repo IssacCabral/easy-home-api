ad550abc973d67937eb69984d5b445e4197373a9
Issac Cabral <clidenorissac@gmail.com>
2024-10-21 14:12:58 -0300

    🐞 fix: resolve error handling

--- src/controllers/components/property/createPropertyController.ts
+++ src/controllers/components/property/createPropertyController.ts
@@ -1,6 +1,9 @@
+import { AmenitiesNotFound } from "@business/errors/amenity";
+import { LandlordNotFound } from "@business/errors/landlord";
+import { CreatePropertyGeneralError } from "@business/errors/property";
 import type { CreatePropertyOperator } from "@controllers/operators/property/createPropertyOperator";
 import type { HttpRequest, HttpResponse } from "@controllers/protocols/http";
-import { badRequest, created, serverError } from "@controllers/protocols/httpStatus";
+import { badRequest, created, notFound, serverError } from "@controllers/protocols/httpStatus";
 import type { IController } from "@controllers/protocols/iController";
 import { InputCreatePropertySerializer } from "@controllers/serializers/property/createPropertySerializer";
 import type { IError } from "@shared/iError";
@@ -15,6 +18,14 @@ export class CreatePropertyController implements IController {
 			const result = await this.operator.exec(input);
 
 			if (result.isLeft()) {
+				if (result.value === CreatePropertyGeneralError) {
+					throw new Error(result.value.message);
+				}
+
+				if (result.value === LandlordNotFound || result.value.code === AmenitiesNotFound().code) {
+					return notFound(result.value);
+				}
+
 				return badRequest(result.value);
 			}
 
