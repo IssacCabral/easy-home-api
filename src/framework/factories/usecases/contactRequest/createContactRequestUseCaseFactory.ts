1fa05348c1e39ec6e76f20f1951c8bb8427bc97a
Issac Cabral <clidenorissac@gmail.com>
2024-11-10 21:46:24 -0300

    🐞 fix: resolve factory

--- src/framework/factories/usecases/contactRequest/createContactRequestUseCaseFactory.ts
+++ src/framework/factories/usecases/contactRequest/createContactRequestUseCaseFactory.ts
@@ -1,9 +1,11 @@
 import { CreateContactRequestUseCase } from "@business/usecases/contactRequest/createContactRequestUseCase";
 import { prismaClient } from "@framework/database/prisma/prismaClient";
 import { ContactRequestRepository } from "@framework/repositories/contactRequestRepository";
+import { PropertyRepository } from "@framework/repositories/propertyRepository";
 
 export const makeCreateContactRequestUseCase = (): CreateContactRequestUseCase => {
 	const contactRequestRepository = new ContactRequestRepository(prismaClient);
+	const propertyRepository = new PropertyRepository(prismaClient);
 
-	return new CreateContactRequestUseCase(contactRequestRepository);
+	return new CreateContactRequestUseCase(contactRequestRepository, propertyRepository);
 };
